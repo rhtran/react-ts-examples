69548fba1430f47b28702ea34418d391bbecc065
Ryan Tran <Ryan.Tran@capgroup.com>
2021-08-29 01:15:41 -0700

    usersState continue....

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,28 @@ import './App.css';
 const App: React.FC = () => {
 
   interface UserInt {
-    name: string,
-    age: number,
-    job: string
+    name: string;
+    age: string;
+    job: string;
   }
 
-  const [usersState, setUsersState] = useState<{currentUser: UserInt}>({
+  interface AllUserInt {
+    currentUser: UserInt;
+    allUsers: Array<UserInt>;
+  }
+
+  const [usersState, setUsersState] = useState<AllUserInt>({
     currentUser: {
       name: "",
-      age: 0,
+      age: "",
       job: ""
-    }
+    },
+    allUsers: []
   })
 
   const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>): void => {
-
     setUsersState({
+      ...usersState,
       currentUser: {
         ...usersState.currentUser,
         [e.target.name]: e.target.value
@@ -27,19 +33,43 @@ const App: React.FC = () => {
     })
   }
 
-  console.log(usersState.currentUser);
+  const allUsers = usersState.allUsers.map((user, i) => (
+    <div key={i}>
+      <h2>{user.name}</h2>
+      <h2>{user.age}</h2>
+      <h2>{user.job}</h2>
+    </div>
+  ))
+
+  console.log(usersState);
+
+  const submitForm = (e: React.SyntheticEvent): void => {
+    e.preventDefault();
+
+    setUsersState({
+      currentUser: {
+        name: "",
+        age: "",
+        job: ""
+      },
+      allUsers: [
+        ...usersState.allUsers,
+        usersState.currentUser
+      ]
+    })
+  }
 
   return (
     <div className="container">
       <h1>React with Typescript</h1>
-      <form>
+      <form onSubmit={submitForm}>
         <label htmlFor="username">Name:</label>
         <input id="username" type="text" name="name"
                value={usersState.currentUser.name}
                onChange={onChangeHandler}/>
 
         <label htmlFor="userAge">Age:</label>
-        <input id="userAge" type="number" name="age"
+        <input id="userAge" type="string" name="age"
                value={usersState.currentUser.age}
                  onChange={onChangeHandler}/>
 
@@ -51,6 +81,7 @@ const App: React.FC = () => {
         <button type="submit">Add User</button>
       </form>
 
+      {allUsers}
     </div>
   )
 
