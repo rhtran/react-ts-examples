72d52d51b915c4270b9787dc6fb44d21e3c85edb
Ryan Tran <Ryan.Tran@capgroup.com>
2021-08-29 00:37:45 -0700

    userState component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,52 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
+
+  interface UserInt {
+    name: string,
+    age: number,
+    job: string
+  }
+
+  const [usersState, setUsersState] = useState<{currentUser: UserInt}>({
+    currentUser: {
+      name: "",
+      age: 0,
+      job: ""
+    }
+  })
+
+  const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>): void => {
+
+    setUsersState({
+      currentUser: {
+        ...usersState.currentUser,
+        [e.target.name]: e.target.value
+      }
+    })
+  }
+
+  console.log(usersState.currentUser);
 
   return (
     <div className="container">
       <h1>React with Typescript</h1>
       <form>
         <label htmlFor="username">Name:</label>
-        <input id="username" type="text" name="name" value=""/>
+        <input id="username" type="text" name="name"
+               value={usersState.currentUser.name}
+               onChange={onChangeHandler}/>
 
         <label htmlFor="userAge">Age:</label>
-        <input id="userAge" type="number" name="age" value=""/>
+        <input id="userAge" type="number" name="age"
+               value={usersState.currentUser.age}
+                 onChange={onChangeHandler}/>
 
         <label htmlFor="userJob">Job:</label>
-        <input id="userJob" type="text" name="job" value=""/>
+        <input id="userJob" type="text" name="job"
+               value={usersState.currentUser.job}
+               onChange={onChangeHandler}/>
 
         <button type="submit">Add User</button>
       </form>
